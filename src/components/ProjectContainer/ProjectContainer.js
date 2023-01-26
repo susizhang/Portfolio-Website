ebb159f47e0a8709dd442bb11a5097e8dfabfffa
suqun zhang <susizhang2017@gmail.com>
2023-01-26 13:56:47 +0100

    Adding image to projects

--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -4,21 +4,13 @@ import LaunchIcon from "@mui/icons-material/Launch";
 import "./ProjectContainer.css";
 
 const ProjectContainer = ({ project, jumpTo }) => {
- 
   return (
     <div className="project">
+      <img src={project.imageUrl} alt="project-img" className="project-img" />
+
       <h3>{project.name}</h3>
 
       <p className="project__description">{project.description}</p>
-      {project.stack && (
-        <ul className="project__stack">
-          {project.stack.map((item) => (
-            <li key={uniqid()} className="project__stack-item">
-              {item}
-            </li>
-          ))}
-        </ul>
-      )}
 
       {project.sourceCode && (
         <button
