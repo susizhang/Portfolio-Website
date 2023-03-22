032cb626c162d55d6c9ccb2a87c36d320ac8fcd3
suqun zhang <susizhang2017@gmail.com>
2023-03-22 11:34:21 +0100

    changing link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => (
   <footer className="footer">
     <a
-      href="https://github.com/rajshekhar26/cleanfolio"
+      href="https://github.com/susizhang/German_Mindmap_Dictionary_frontend"
       className="link footer__link"
     >
       Created By Susi Zhang
